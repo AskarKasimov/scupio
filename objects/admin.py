fddc606346898fd81603c2cc7fe99825b6668327
«vvych» <wychelm@ya.ru>
2022-11-24 19:12:51 +0700

    1.4 fix models

--- objects/admin.py
+++ objects/admin.py
@@ -2,10 +2,20 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Object)
-admin.site.register(Lab)
-admin.site.register(LabObject)
-admin.site.register(Field)
+class LabObjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'author',
+    )
+
+
+admin.site.register(OilField)
 admin.site.register(Well)
 admin.site.register(Layer)
+
+admin.site.register(Lab)
+admin.site.register(Object)
+admin.site.register(LabObject, LabObjectAdmin)
 admin.site.register(Task)
+
+
--- objects/models.py
+++ objects/models.py
@@ -3,104 +3,106 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class Object(models.Model):
-    name = models.IntegerField(verbose_name='Номер объекта')
-    field = models.IntegerField(verbose_name='Номер месторождения')
-    income_date = models.DateTimeField(auto_now_add=True, verbose_name='Время получения')
+
+# Пласт
+class Layer(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название пласта')
 
     def __str__(self) -> str:
         return str(self.name)
-    
 
-class Lab(models.Model):
-    name = models.IntegerField(verbose_name='Номер лаборатории')
-    start_date = models.DateTimeField(auto_now_add=True, verbose_name='Время старта работы')
-    end_date = models.DateTimeField(auto_now_add=True, verbose_name='Время конца работы')
-    author = models.ForeignKey(
-        User,
-        related_name='lab',
-        verbose_name='Оператор',
-        on_delete=models.CASCADE,
+
+# Скважина
+class Well(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Номер скважины')
+    layer = models.ForeignKey(
+        Layer,
+        related_name='layers',
+        verbose_name='Пласт',
+        on_delete=models.CASCADE,  
         default = 1
     )
 
-    task = models.IntegerField(verbose_name='Номер работы')
-
     def __str__(self) -> str:
         return str(self.name)
 
 
-class LabObject(models.Model):
-    object = models.ForeignKey(
-        Object,
-        related_name='lab',
-        verbose_name='Логи объекта',
-        on_delete=models.CASCADE
-        
-    )
-    laboratory = models.ForeignKey(
-        Lab,
-        related_name='lab',
-        verbose_name='Логи лаборатории',
-        on_delete=models.CASCADE
-    )
-
-    def __str__(self) -> str:
-        return (str(self.object.name) + ':' + str(self.laboratory.name))
-
-
-class Field(models.Model):
-    field_well = models.ForeignKey(
-        Object,
-        related_name='oilfield', ##
-        verbose_name='Родительское месторождение',
-        on_delete=models.CASCADE,
-        default=0
-    )
-    name = models.CharField(max_length=200, verbose_name='Название скважины')
-
-    def __str__(self) -> str:
-        return str(self.name)
-
-    
-class Well(models.Model):
-    well_layer = models.ForeignKey(
-        Field,
-        related_name='oilfield', ##
-        verbose_name='Родиетельская скважина',
-        on_delete=models.CASCADE,
-        default=0
-    )
-    name = models.CharField(max_length=200, verbose_name='Название пласта')
-
-    def __str__(self) -> str:
-        return str(self.name)
-    
-
-class Layer(models.Model):
-    layer = models.ForeignKey(
+# Месторождение
+class OilField(models.Model): 
+    name = models.CharField(max_length=200, verbose_name='Название месторождения')
+    well = models.ForeignKey(
         Well,
-        related_name='oilfield',
-        verbose_name='Пласт',
-        on_delete=models.CASCADE
+        related_name='fields',
+        verbose_name='Скважина',
+        on_delete=models.CASCADE,  
+        default = 1
     )
-    name = models.CharField(max_length=200, verbose_name='Название пласта')
+    def __str__(self) -> str:
+        return str(self.name)
+
+
+# Лаборатория
+class Lab(models.Model):
+    name = models.IntegerField(verbose_name='Номер лаборатории')
 
     def __str__(self) -> str:
         return str(self.name)
 
 
-class Task(models.Model):
-    task_lab = models.ForeignKey(
-        Lab,
-        related_name='taskforlab',
-        verbose_name='Lab',
-        on_delete=models.CASCADE
+# Объект
+class Object(models.Model):
+    name = models.IntegerField(verbose_name='Номер объекта')
+    oilfield = models.ForeignKey(
+        OilField,
+        related_name='objects',
+        verbose_name='Номер месторождения',
+        on_delete=models.CASCADE,  
+        default = 1
     )
+    income_date = models.DateTimeField(verbose_name='Время получения')
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+
+# Исследование
+class Task(models.Model):
     type = models.IntegerField(verbose_name='Тип работы')
     result = models.CharField(max_length=200, verbose_name='Результат работы')
 
     def __str__(self) -> str:
         return str(self.result)
 
-    
+
+# Список исследований
+class LabObject(models.Model):
+    object = models.ForeignKey(
+        Object,
+        related_name='lab_object',
+        verbose_name='Объект',
+        on_delete=models.CASCADE
+    )
+    laboratory = models.ForeignKey(
+        Lab,
+        related_name='lab_object',
+        verbose_name='Лаборатория',
+        on_delete=models.CASCADE
+    )
+    start_date = models.DateTimeField(verbose_name='Время старта работы')
+    end_date = models.DateTimeField(auto_now_add=True, verbose_name='Время конца работы')
+    author = models.ForeignKey(
+        User,
+        related_name='lab_object',
+        verbose_name='Оператор',
+        on_delete=models.CASCADE,  
+        default = 1
+    )
+    task = models.ForeignKey(
+        Task,
+        related_name='lab_object',
+        verbose_name='Исследование',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self) -> str:
+        return (str(self.object.name) + ':' + str(self.laboratory.name))
